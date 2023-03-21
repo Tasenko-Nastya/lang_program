f480b6ed775b2e21a183ab65d552547d5acb7b3e
Anastasiya <Fankovskaya-asya@mail.ru>
2023-03-21 21:47:03 +0300

    random

--- proect3_sum/Program.cs
+++ proect3_sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿nt numberA = new Random().Next(1,10); // 1 2 3 4...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA+numberB);
+Console.WriteLine(result);
