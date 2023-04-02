011e16c7c620a1d88da8a11361691c0fae23a172
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-02 14:55:39 +0300

    Функции

--- proect3_sum/Program.cs
+++ proect3_sum/Program.cs
@@ -1,6 +1,6 @@
-﻿nt numberA = new Random().Next(1,10); // 1 2 3 4...9
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4...9
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
 Console.WriteLine(numberA+numberB);
-Console.WriteLine(result);
+
