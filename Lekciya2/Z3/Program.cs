5da9a5a3351808472aa4e259c62cd4498444ba46
Anastasiya <Fankovskaya-asya@mail.ru>
2023-04-02 15:39:07 +0300

    нахождение по индексу

---
+++ Lekciya2/Z3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {11, 21, 31, 146, 25, 61, 17, 38, }; 
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        break;
+    }
+    index++;
+}
